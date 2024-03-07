12d02186ee1e8290f85a0a3e03415ed161be47c9
Ron Friedman <ronskons@students.cs.ubc.ca>
2024-03-07 15:52:54 -0800

    Add GUI elements

---
+++ htdocs/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CREATABLES</title>
+    <style>
+        body {
+            text-align: center;
+        }
+        table {
+            border: 1px solid black;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        td {
+            border-top: 1px solid black;
+            border-bottom: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body style="text-align: center">
+    <h1>CREATABLES</h1>
+    <button>LOG IN</button>
+    <h3>Recent projects:</h3>
+    <table>
+        <tr>
+            <td><a href="#">How to grow big beans</a></td>
+            <td><br>JSKONS</td>
+        </tr>
+        <tr>
+            <td><a href="#">Grandma's smoothie recipe</a></td>
+            <td><br>G-dawg</td>
+        </tr>
+        <tr>
+            <td><a href="#">Build a robot in 10 mins</a></td>
+            <td><br>bingwad</td>
+        </tr>
+        <tr>
+            <td><a href="#">Lorem ipsum dolor</a></td>
+            <td><br>JSKONS</td>
+        </tr>
+    </table>
+</body>
+</html>
