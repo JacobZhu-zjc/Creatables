95aabc752475e65d750f0b853cc8b197767dd065
Jacob Zhu <jzhu46@students.cs.ubc.ca>
2024-03-07 16:24:47 -0800

    Added the COMPOSE MESSAGE page

---
+++ htdocs/compose_message.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>LOGIN</title>
+    <style>
+        body {
+            text-align: center;
+        }
+        label {
+            text-align: left;
+        }
+        input {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+<h1>LOGIN</h1>
+<form action="#">
+    <label for="recipient">Recipent:</label>
+    <br>
+    <input type="text" name="recipient" id="recipient" placeholder="Username">
+    <br>
+    <label for="subject">Subject:</label>
+    <br>
+    <input type="text" name="subject" id="subject" placeholder="Pick an interesting title">
+    <br>
+    <label for="message">Subject:</label>
+    <br>
+    <input type="text" name="message" id="message" placeholder="Say something nice...">
+    <br>
+    <input type="submit" value="SEND">
+</form>
+</body>
+</html>
